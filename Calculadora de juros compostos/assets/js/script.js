9d7b7661acb0c21497a745ab6003366c7e5898b7
Thiago Cajaiba <thiago.cajaiba@gmail.com>
2023-01-12 10:00:23 -0300

    Separada função da chamada no EventListener

--- Calculadora de juros compostos/assets/js/script.js
+++ Calculadora de juros compostos/assets/js/script.js
@@ -1,9 +1,14 @@
-document.getElementById('calculate').addEventListener('click', function(){
-    const value = document.getElementById('value').value;
-    const fee = (document.getElementById('fee').value) / 100;
-    const time = document.getElementById('time').value;
+document
+  .getElementById("calculate")
+  .addEventListener("click", calcCompoundInterest);
 
-    const total = value * (1 + fee)**time;
+function calcCompoundInterest() {
+  const value = document.getElementById("value").value;
+  const fee = document.getElementById("fee").value / 100;
+  const time = document.getElementById("time").value;
 
-    document.getElementById('total').innerHTML = ("R$ " + total.toFixed(2).replace('.', ','));
-});
+  const total = value * (1 + fee) ** time;
+
+  document.getElementById("total").innerHTML =
+    "R$ " + total.toFixed(2).replace(".", ",");
+}
