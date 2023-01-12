5991ad89cd3e379f422c99be0685b4109290ebb9
Thiago Cajaiba <thiago.cajaiba@gmail.com>
2023-01-12 10:13:19 -0300

    Organização de arquivos

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+document
+  .getElementById("calculate")
+  .addEventListener("click", calcCompoundInterest);
+
+function calcCompoundInterest() {
+  const value = document.getElementById("value").value;
+  const fee = document.getElementById("fee").value / 100;
+  const time = document.getElementById("time").value;
+
+  const total = value * (1 + fee) ** time;
+
+  document.getElementById("total").innerHTML =
+    "R$ " + total.toFixed(2).replace(".", ",");
+}
